8bff0866f000c8f59d788fe581097ce0f8c5dd7f
Teotia444 <comptest444@gmail.com>
2024-06-22 20:00:50 +0200

    Shameless code steal

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WWHDR_configloader
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.wiiuIpTextbox = new System.Windows.Forms.TextBox();
+			this.toWiiU = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.toPc = new System.Windows.Forms.Button();
+			this.pathTextbox = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.ipCheck = new System.Windows.Forms.Button();
+			this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.message = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// wiiuIpTextbox
+			// 
+			this.wiiuIpTextbox.Location = new System.Drawing.Point(32, 47);
+			this.wiiuIpTextbox.Name = "wiiuIpTextbox";
+			this.wiiuIpTextbox.Size = new System.Drawing.Size(210, 20);
+			this.wiiuIpTextbox.TabIndex = 0;
+			this.wiiuIpTextbox.TextChanged += new System.EventHandler(this.wiiuIpTextbox_TextChanged);
+			// 
+			// toWiiU
+			// 
+			this.toWiiU.Location = new System.Drawing.Point(56, 256);
+			this.toWiiU.Name = "toWiiU";
+			this.toWiiU.Size = new System.Drawing.Size(210, 56);
+			this.toWiiU.TabIndex = 1;
+			this.toWiiU.Text = "PC -> Wii U";
+			this.toWiiU.UseVisualStyleBackColor = true;
+			this.toWiiU.Click += new System.EventHandler(this.toWiiU_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(29, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(86, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Wii U IP address";
+			// 
+			// toPc
+			// 
+			this.toPc.Location = new System.Drawing.Point(56, 186);
+			this.toPc.Name = "toPc";
+			this.toPc.Size = new System.Drawing.Size(210, 54);
+			this.toPc.TabIndex = 3;
+			this.toPc.Text = "Wii U -> PC";
+			this.toPc.UseVisualStyleBackColor = true;
+			this.toPc.Click += new System.EventHandler(this.toPc_Click);
+			// 
+			// pathTextbox
+			// 
+			this.pathTextbox.Location = new System.Drawing.Point(32, 94);
+			this.pathTextbox.Name = "pathTextbox";
+			this.pathTextbox.Size = new System.Drawing.Size(210, 20);
+			this.pathTextbox.TabIndex = 4;
+			this.pathTextbox.TextChanged += new System.EventHandler(this.pathTextbox_TextChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(29, 78);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(154, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Path to the desktop application";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(249, 90);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(59, 23);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "Browse";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// ipCheck
+			// 
+			this.ipCheck.Location = new System.Drawing.Point(248, 45);
+			this.ipCheck.Name = "ipCheck";
+			this.ipCheck.Size = new System.Drawing.Size(59, 23);
+			this.ipCheck.TabIndex = 7;
+			this.ipCheck.Text = "Check";
+			this.ipCheck.UseVisualStyleBackColor = true;
+			this.ipCheck.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// fileDialog
+			// 
+			this.fileDialog.Filter = "WWHD Rando EXE (wwhd_rando.exe)|*.exe";
+			this.fileDialog.RestoreDirectory = true;
+			// 
+			// message
+			// 
+			this.message.AutoSize = true;
+			this.message.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.message.Location = new System.Drawing.Point(29, 143);
+			this.message.Name = "message";
+			this.message.Size = new System.Drawing.Size(0, 13);
+			this.message.TabIndex = 8;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(312, 356);
+			this.Controls.Add(this.message);
+			this.Controls.Add(this.ipCheck);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pathTextbox);
+			this.Controls.Add(this.toPc);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.toWiiU);
+			this.Controls.Add(this.wiiuIpTextbox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Config Transfer";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox wiiuIpTextbox;
+		private System.Windows.Forms.Button toWiiU;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button toPc;
+		private System.Windows.Forms.TextBox pathTextbox;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button ipCheck;
+		private System.Windows.Forms.OpenFileDialog fileDialog;
+		private System.Windows.Forms.Label message;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WWHDR_configloader
+{
+
+	public partial class Form1 : Form
+	{
+		string dateformat = "dd/MM/yyyy";
+		string ampm = "";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (fileDialog.ShowDialog() == DialogResult.OK)
+			{
+				pathTextbox.Text = fileDialog.FileName;
+			}
+		}
+
+
+		private (string[], List<DateTime>) readWiiUFolder(string path, int timeout)
+		{
+			// Get the object used to communicate with the server.
+			FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://"+ wiiuIpTextbox.Text + ":21/" + path);
+			request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+			request.Timeout = timeout;
+			// This example assumes the FTP site uses anonymous logon.
+			request.Credentials = new NetworkCredential("anonymous", "whatever");
+
+			FtpWebResponse response = null;
+			try { response = (FtpWebResponse)request.GetResponse(); }
+			catch (WebException e){ MessageBox.Show("Invalid IP address"); return (new string[0], new List<DateTime>()); }
+			
+			Stream responseStream = response.GetResponseStream();
+			StreamReader reader = new StreamReader(responseStream);
+			string pattern =
+	@"^([\w-]+)\s+(\d+)\s+(\w+)\s+(\w+)\s+(\d+)\s+" +
+	@"(\w+\s+\d+\s+\d+|\w+\s+\d+\s+\d+:\d+)\s+(.+)$";
+			Regex regex = new Regex(pattern);
+			IFormatProvider culture = CultureInfo.GetCultureInfo("en-us");
+			string[] hourMinFormats =
+				new[] { "MMM dd HH:mm", "MMM dd H:mm", "MMM dd HH:mm", "MMM dd H:mm" };
+			string[] yearFormats =
+				new[] { "MMM dd yyyy", "MMM dd yyyy" };
+
+			List<string> infos = new List<string>();
+			List<DateTime> dates = new List<DateTime>();
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				Match match = regex.Match(line);
+				string permissions = match.Groups[1].Value;
+				int inode = int.Parse(match.Groups[2].Value, culture);
+				string owner = match.Groups[3].Value;
+				string group = match.Groups[4].Value;
+				long size = long.Parse(match.Groups[5].Value, culture);
+				string s = Regex.Replace(match.Groups[6].Value, @"\s+", " ");
+
+				string[] formats = (s.IndexOf(':') >= 0) ? hourMinFormats : yearFormats;
+				dates.Add(DateTime.ParseExact(s, formats, culture, DateTimeStyles.None));
+				string name = match.Groups[7].Value;
+				infos.Add(name);
+				
+			}
+			return (infos.ToArray(), dates);
+
+		}
+		private string readWiiUFolderString(string path, int timeout)
+		{
+			// Get the object used to communicate with the server.
+			FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + wiiuIpTextbox.Text + ":21/" + path);
+			request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+			request.Timeout = timeout;
+			// This example assumes the FTP site uses anonymous logon.
+			request.Credentials = new NetworkCredential("anonymous", "whatever");
+
+			FtpWebResponse response = null;
+			try
+			{
+				response = (FtpWebResponse)request.GetResponse();
+				Stream responseStream = response.GetResponseStream();
+				StreamReader reader = new StreamReader(responseStream);
+				var items = reader.ReadToEnd();
+				reader.Close();
+				response.Close();
+				return items;
+
+			}
+			catch (System.Net.WebException e)
+			{
+
+				response = (FtpWebResponse)e.Response;
+				if (response.StatusCode == FtpStatusCode.ActionNotTakenFileUnavailable)
+				{
+					return "FileNull";
+				}
+				else
+				{
+					return "ServerUnk";
+				}
+			}
+		}
+
+		private string findWiiUPath()
+		{
+			List<string[]> eachFolder = new List<string[]>();
+			(var wiiufolderName, var wiiufolderDateTime) = readWiiUFolder("fs/vol/external01/wiiu/apps/save/", 1000);
+			if (wiiufolderName != null)
+			{ 
+				string pathRecent = "";
+				pathRecent = "B318AE3B (TWWHD Randomizer)";
+				DateTime dateRecent = new DateTime(1970, 01, 01, 01, 00, 00);
+				Console.WriteLine(wiiufolderName.Length);
+				for (int i = 0; i<wiiufolderName.Length; i++)
+				{
+					Console.WriteLine(wiiufolderName[i]);
+					if (wiiufolderName[i].Contains("(TWWHD Randomizer)"))
+					{
+
+						if (wiiufolderDateTime[i] > dateRecent)
+						{
+							pathRecent = wiiufolderName[i];
+							dateRecent = wiiufolderDateTime[i];
+						}
+					}
+				}
+				return "fs/vol/external01/wiiu/apps/save/" + pathRecent;
+			}
+			return "ErrNoFolderFound";
+
+
+		}
+		public void download(string remoteFile, string localFile)
+		{
+			try
+			{
+				if(remoteFile == "ErrNoFolderFound/config.yaml") {
+					return;
+				}
+				/* Create an FTP Request */
+				FtpWebRequest ftpRequest = (FtpWebRequest)FtpWebRequest.Create("ftp://" + wiiuIpTextbox.Text + ":21/" + remoteFile);
+				/* Log in to the FTP Server with the User Name and Password Provided */
+				ftpRequest.Credentials = new NetworkCredential("anonymous", "whatever");
+				/* When in doubt, use these options */
+				ftpRequest.UseBinary = true;
+				ftpRequest.UsePassive = true;
+				ftpRequest.KeepAlive = true;
+				/* Specify the Type of FTP Request */
+				ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+				/* Establish Return Communication with the FTP Server */
+				FtpWebResponse ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+				/* Get the FTP Server's Response Stream */
+				Stream ftpStream = ftpResponse.GetResponseStream();
+				/* Open a File Stream to Write the Downloaded File */
+				FileStream localFileStream = new FileStream(localFile, FileMode.Create);
+				/* Buffer for the Downloaded Data */
+				byte[] byteBuffer = new byte[2048];
+				int bytesRead = ftpStream.Read(byteBuffer, 0, 2048);
+				/* Download the File by Writing the Buffered Data Until the Transfer is Complete */
+				try
+				{
+					while (bytesRead > 0)
+					{
+						localFileStream.Write(byteBuffer, 0, bytesRead);
+						bytesRead = ftpStream.Read(byteBuffer, 0, 2048);
+					}
+				}
+				catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+				/* Resource Cleanup */
+				localFileStream.Close();
+				ftpStream.Close();
+				ftpResponse.Close();
+				ftpRequest = null;
+			}
+			catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+			return;
+		}
+		public void upload(string remoteFile, string localFile)
+		{
+			try
+			{
+				/* Create an FTP Request */
+				FtpWebRequest ftpRequest = (FtpWebRequest)FtpWebRequest.Create("ftp://" + wiiuIpTextbox.Text + ":21/"+ remoteFile);
+				/* Log in to the FTP Server with the User Name and Password Provided */
+				ftpRequest.Credentials = new NetworkCredential("anonymous", "whatever");
+				/* When in doubt, use these options */
+				ftpRequest.UseBinary = true;
+				ftpRequest.UsePassive = true;
+				ftpRequest.KeepAlive = true;
+				/* Specify the Type of FTP Request */
+				ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+				/* Establish Return Communication with the FTP Server */
+				Stream ftpStream = ftpRequest.GetRequestStream();
+				/* Open a File Stream to Read the File for Upload */
+				FileStream localFileStream = new FileStream(localFile, FileMode.Open);
+				/* Buffer for the Downloaded Data */
+				byte[] byteBuffer = new byte[2048];
+				int bytesSent = localFileStream.Read(byteBuffer, 0, 2048);
+				/* Upload the File by Sending the Buffered Data Until the Transfer is Complete */
+				try
+				{
+					while (bytesSent != 0)
+					{
+						ftpStream.Write(byteBuffer, 0, bytesSent);
+						bytesSent = localFileStream.Read(byteBuffer, 0, 2048);
+					}
+				}
+				catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+				/* Resource Cleanup */
+				localFileStream.Close();
+				ftpStream.Close();
+				ftpRequest = null;
+			}
+			catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+			return;
+		}
+
+
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			var response = readWiiUFolderString("fs/vol/external01/wiiu/apps/save", 1000);
+			if (response == "FileNull")
+			{
+				MessageBox.Show("The program was able to connect to the server, but no files were detected. Make sure to launch the Rando app at least once to generate a config file.");
+			}
+			else if (response == "ServerUnk")
+			{
+				MessageBox.Show("The program was unable to connect to the server. Make sure you entered the correct address (I.e. 192.168.XXX.XXX) without including the port (numbers after the colon)");
+			}
+			else
+			{
+				message.Text = "The Wii U responded correctly!";
+			}
+		}
+
+		private void toPc_Click(object sender, EventArgs e)
+		{
+			if(wiiuIpTextbox.Text == "")
+			{
+				MessageBox.Show("Please enter a valid IP address");
+				return;
+			}
+			if (pathTextbox.Text == "")
+			{
+				MessageBox.Show("Please enter a path for the PC app");
+				return;
+			}
+			download(findWiiUPath() + "/config.yaml", pathTextbox.Text.Replace(@"wwhd_rando.exe", string.Empty) + "config.yaml");
+			message.Text = "Success!";
+		}
+
+		private void toWiiU_Click(object sender, EventArgs e)
+		{
+			if (wiiuIpTextbox.Text == "")
+			{
+				MessageBox.Show("Please enter a valid IP address");
+				return;
+			}
+			if (pathTextbox.Text == "")
+			{
+				MessageBox.Show("Please enter a path for the PC app");
+				return;
+			}
+			if(!File.Exists(pathTextbox.Text.Replace(@"wwhd_rando.exe", string.Empty) + "config.yaml"))
+			{
+				MessageBox.Show("The config file does not exists on your computer. Make sure that you opened and closed the app at least once.");
+				return;
+			}
+			upload(findWiiUPath() + "/config.yaml", pathTextbox.Text.Replace(@"wwhd_rando.exe", string.Empty) + "config.yaml");
+			message.Text = "Success!";
+		}
+
+		private void wiiuIpTextbox_TextChanged(object sender, EventArgs e)
+		{
+			Properties.Settings.Default.ipaddress = wiiuIpTextbox.Text;
+			Properties.Settings.Default.Save();
+		}
+
+		private void pathTextbox_TextChanged(object sender, EventArgs e)
+		{
+			Properties.Settings.Default.pathpc = pathTextbox.Text;
+			Properties.Settings.Default.Save();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			pathTextbox.Text = Properties.Settings.Default.pathpc;
+			wiiuIpTextbox.Text = Properties.Settings.Default.ipaddress;
+			Console.WriteLine(CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern);
+			if (CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern == "M/d/yyyy" || CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern == "MM/dd/yyyy")
+			{
+				dateformat = "MM/dd/yyyy";
+				ampm = "a";
+			}
+			else
+			{
+				dateformat = CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern;
+			}
+		}
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			MessageBox.Show(CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern);
+		}
+	}
+}
